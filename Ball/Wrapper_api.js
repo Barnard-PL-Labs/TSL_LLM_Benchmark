932714c1fe74900f5e46639c581a6a046e7b8056
w <no>
2024-04-08 20:48:22 -0400

    add ball

---
+++ Ball/Wrapper_api.js
@@ -0,0 +1,10 @@
+
+/*
+
+"ball" is an object with properties:
+	
+	ball.ctrl => this receives the 
+
+	ball.position() => this returns the position of the ball as a number between 0 and 10. 0 represents that the ball is against the leftmost wall and 10 represents that the ball is against the rightmost wall.
+
+*/
