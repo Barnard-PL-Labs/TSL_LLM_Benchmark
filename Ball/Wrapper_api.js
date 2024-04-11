17a1868266bf1f8f9477c23154d57b078423ff9d
w <no>
2024-04-10 21:05:39 -0400

    get run to work

--- Ball/Wrapper_api.js
+++ Ball/Wrapper_api.js
@@ -1,10 +1,6 @@
 
 /*
 
-"ball" is an object with properties:
-	
-	ball.ctrl => this receives the 
-
-	ball.position() => this returns the position of the ball as a number between 0 and 10. 0 represents that the ball is against the leftmost wall and 10 represents that the ball is against the rightmost wall.
+Get or set the position of the ball by changing "ball.pos"
 
 */
--- run.py
+++ run.py
@@ -6,7 +6,11 @@ This is a work in progress
 
 """
 
+import os
 import argparse
+from subprocess import check_call
+
+file_dir = os.path.dirname(__file__)
 
 parser = argparse.ArgumentParser(description="Generate a program using TSL")
 parser.add_argument('-d', '--dir', required=True, help='Directory of the prompts to generate from')
@@ -18,30 +22,37 @@ to_interpolate = {
     'functions_and_predicates': 'Headers.txt',
     'natural_language_summary': 'NL.summary.txt',
     'natural_language_description': 'NL.txt',
+    'wrapper_api': 'Wrapper_api.js'
 }
-
-# def should_use_original(name):
-#     return (
-#         name in [ 'NL.summary.txt', 'functions_and_predicates' ] ||
-#         args.method == 'nl' && 'functions_and_predicates' ||
-#         args.emthod
 
 def load_file_and_interpolate(filename):
     with open(filename) as file:
         result = file.read()
         for (name, filename) in to_interpolate.items():
-            if should_use_original(name):
-                path = args.dir + '/' + filename
-            else:
-                path = args.dir + '/computed/' + filename
+            path = args.dir + '/' + filename
             with open(path) as file:
                 replacement = file.read()
             for to_replace in [ name.upper() + '_GO_HERE', name.upper() + '_GOES_HERE' ]:
-                result.replace_all(to_replace, replacement)
+                result.replace(to_replace, replacement)
     return result
 
-"""
+if args.method == 'nl':
+    spec_prompt_filename = args.dir + '/computed/Spec.prompt'
+    with open(spec_prompt_filename, 'w') as file:
+        file.write(load_file_and_interpolate(file_dir + '/Spec.prompt'))
+    spec_filename = args.dir + '/computed/Spec.tsl'
+    synth_filename = args.dir + '/computed/Synth.js'
+    print(f'Please paste the contents of this file into the TSL GPT:\n\n    {spec_prompt_filename}\n\nThen, paste the spec in the response into:\n\n    {spec_filename}\n\nWhen you have done this, press Enter')
+    input()
+    check_call([ 'tsl', 'synthesize', '-i', spec_filename, '--js', '-o', synth_filename ])
+else:
+    spec_filename = args.dir + '/Spec.tsl'
+    raise Exception('the only method implemented is "nl". the others are TODO.')
 
-tsl synthesize -i TSL_LLM_Benchmark/llm-descriptions/Ball/computed/Spec.tsl --js -o TSL_LLM_Benchmark/llm-descriptions/Ball/computed/Synth.js
+impl_prompt_filename = args.dir + '/computed/Impl.prompt'
+impl_filename = args.dir + '/computed/Impl.js'
+with open(impl_prompt_filename, 'w') as file:
+    file.write(load_file_and_interpolate(file_dir + '/Impl.prompt'))
 
-"""
+print(f'Please paste the contents of this file into the TSL GPT:\n\n    {impl_prompt_filename}\n\nThen, paste the spec in the response into:\n\n    {impl_filename}\n\nWhen you have done this, press Enter')
+
